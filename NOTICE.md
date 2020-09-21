66fb30b7ae27bcc4c693891d60d2bd71fb8cc2de
sedaboni <andrea.sebastiani2@studio.unibo.it>
2020-09-21 22:55:52 +0200

    Upload all the files

---
+++ NOTICE.md
@@ -0,0 +1,2 @@
+This repository contains Deep Image Prior code, previously licensed under Apache 2.0
+https://github.com/DmitryUlyanov/deep-image-prior
---
+++ utils/utils.py
@@ -0,0 +1,117 @@
+import numpy as np
+from scipy import signal
+import torch
+
+
+
+def zero_pad(image, shape, position='corner'):
+    """
+    Extends image to a certain size with zeros
+    Parameters
+    ----------
+    image: real 2d `numpy.ndarray`
+        input_image image
+    shape: tuple of int
+        Desired output shape of the image
+    position : str, optional
+        The position of the input_image image in the output one:
+            * 'corner'
+                top-left corner (default)
+            * 'center'
+                centered
+    Returns
+    -------
+    padded_img: real `numpy.ndarray`
+        The zero-padded image
+    """
+    shape = np.asarray(shape, dtype=int)
+    imshape = np.asarray(image.shape, dtype=int)
+
+    if np.alltrue(imshape == shape):
+        return image
+
+    if np.any(shape <= 0):
+        raise ValueError("ZERO_PAD: null or negative shape given")
+
+    dshape = shape - imshape
+    if np.any(dshape < 0):
+        raise ValueError("ZERO_PAD: target size smaller than source one")
+
+    pad_img = np.zeros(shape, dtype=image.dtype)
+
+    idx, idy = np.indices(imshape)
+
+    if position == 'center':
+        if np.any(dshape % 2 != 0):
+            raise ValueError("ZERO_PAD: source and target shapes "
+                             "have different parity.")
+        offx, offy = dshape // 2
+    else:
+        offx, offy = (0, 0)
+
+    pad_img[idx + offx, idy + offy] = image
+
+    return pad_img
+
+def psf2otf(psf, shape):
+    """
+    Convert point-spread function to optical transfer function.
+    Compute the Fast Fourier Transform (FFT) of the point-spread
+    function (PSF) array and creates the optical transfer function (OTF)
+    array that is not influenced by the PSF off-centering.
+    By default, the OTF array is the same size as the PSF array.
+    To ensure that the OTF is not altered due to PSF off-centering, PSF2OTF
+    post-pads the PSF array (down or to the right) with zeros to match
+    dimensions specified in OUTSIZE, then circularly shifts the values of
+    the PSF array up (or to the left) until the central pixel reaches (1,1)
+    position.
+    Parameters
+    ----------
+    psf : `numpy.ndarray`
+        PSF array
+    shape : int
+        Output shape of the OTF array
+    Returns
+    -------
+    otf : `numpy.ndarray`
+        OTF array
+    Notes
+    -----
+    Adapted from MATLAB psf2otf function
+    """
+    if np.all(psf == 0):
+        return np.zeros_like(psf)
+
+    inshape = psf.shape
+    # Pad the PSF to outsize
+    psf = zero_pad(psf, shape, position='corner')
+
+    # Circularly shift OTF so that the 'center' of the PSF is
+    # [0,0] element of the array
+    for axis, axis_size in enumerate(inshape):
+        psf = np.roll(psf, -int(axis_size / 2), axis=axis)
+
+    # Compute the OTF
+    otf = np.fft.fft2(psf)
+
+    # Estimate the rough number of operations involved in the FFT
+    # and discard the PSF imaginary part if within roundoff error
+    # roundoff error  = machine epsilon = sys.float_info.epsilon
+    # or np.finfo().eps
+    n_ops = np.sum(psf.size * np.log2(psf.shape))
+    otf = np.real_if_close(otf, tol=n_ops)
+
+    return otf
+
+def gaussian_kernel(radius, sigma):
+    intermidiate = signal.gaussian(1+2*radius, sigma)
+    kernel = np.outer(intermidiate, intermidiate)
+
+    return kernel
+
+def D(x, Dh_DFT, Dv_DFT):
+    x_DFT = torch.rfft(x, signal_ndim=1, onesided=False)
+    x_DFT = torch.view_as_complex(x_DFT).cuda()
+    Dh_x = torch.irfft(torch.view_as_real(Dh_DFT*x_DFT), signal_ndim=1, onesided=False)
+    Dv_x = torch.irfft(torch.view_as_real(Dv_DFT*x_DFT), signal_ndim=1, onesided=False)
+    return Dh_x, Dv_x
